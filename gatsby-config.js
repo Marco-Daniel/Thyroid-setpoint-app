c9e85d9ac86c229a3d8de4491629d940d468ffe4
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-05-20 16:30:25 +0200

    cleaned up gatsby boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Thyroid Setpoint Calculator`,
+    description: `Use this app to calculate your setpoint`,
+    author: `M.D. Leguijt, M.D. Design & Development`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,12 +19,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `Setpoint Calculator`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
