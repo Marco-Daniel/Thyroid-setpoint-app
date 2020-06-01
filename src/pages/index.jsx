784642179bee476a451f9697e7a491c18bff95d9
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-06-01 17:56:27 +0200

    basic structure

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import SetpointApp from "../app"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Thyroid Setpoint</h1>
+    <SEO title="Bereken je setpoint" />
+    <SetpointApp />
   </Layout>
 )
 
