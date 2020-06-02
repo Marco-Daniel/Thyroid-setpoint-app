a038dcfeddf94a1f435d408e53cba69d3c181e9c
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-06-02 14:39:50 +0200

    added setpoint display

--- src/app/screenController.jsx
+++ src/app/screenController.jsx
@@ -1,10 +1,14 @@
 import React from "react"
-import DisplayValues from "./DisplayValues"
+import DisplayValues from "./displayValues"
+import DisplaySetpoint from "./displaySetpoint"
 
 const ScreenController = ({ state, dispatch }) => {
   switch (state.screen) {
     case "user-input":
       return <DisplayValues values={state.values} dispatch={dispatch} />
+
+    case "calculated":
+      return <DisplaySetpoint state={state} dispatch={dispatch} />
 
     default:
       return <DisplayValues values={state.values} dispatch={dispatch} />
