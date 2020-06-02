d56770fc3aa08e6a43a7fb3834e8b83ed7bf15de
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-06-02 14:38:39 +0200

    added back button, add example data and clear all input button

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -1,34 +1,76 @@
-import React from "react"
+import React, { useState } from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
 import IconButton from "@material-ui/core/IconButton"
 import MoreIcon from "@material-ui/icons/MoreVert"
+import ArrowBackIcon from "@material-ui/icons/ArrowBack"
+import Menu from "@material-ui/core/Menu"
+import MenuItem from "@material-ui/core/MenuItem"
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
   title: {
-    flexGrow: 1,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translateX(-50%) translateY(-50%)",
+  },
+  alignRight: {
+    marginRight: 0,
+    marginLeft: "auto",
   },
 }))
 
-const Header = () => {
+const Header = ({ displayBack, dispatch }) => {
   const classes = useStyles()
+  const [anchorEl, setAnchorEl] = useState(null)
+
+  const goToDisplayValues = () => dispatch({ type: "SWITCH_SCREEN", payload: "user-input" })
+
+  const addExampleData = () => {
+    dispatch({ type: "CREATE_EXAMPLE_DATA" })
+    handleClose()
+  }
+  const clearInput = () => {
+    dispatch({ type: "CLEAR_USER_INPUT" })
+    handleClose()
+  }
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
 
   return (
-    <AppBar position="sticky" className={classes.root}>
-      <Toolbar>
-        <Typography variant="h5" component="h1" align="center" className={classes.title}>
-          Setpoint Calculator
-        </Typography>
-        <IconButton aria-label="display more actions" edge="end" color="inherit">
-          <MoreIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="sticky" className={classes.root}>
+        <Toolbar>
+          {displayBack && (
+            <IconButton aria-label="display more actions" edge="start" color="inherit" onClick={goToDisplayValues}>
+              <ArrowBackIcon />
+            </IconButton>
+          )}
+          <Typography variant="h5" component="h1" align="center" className={classes.title}>
+            Setpoint Calculator
+          </Typography>
+          <IconButton aria-label="display more-actions" aria-haspopup="true" onClick={handleClick} color="inherit" className={classes.alignRight}>
+            <MoreIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <Menu id="more-actions" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+        <MenuItem onClick={addExampleData}>Voorbeeld data</MenuItem>
+        <MenuItem onClick={clearInput}>Verwijder alle data</MenuItem>
+      </Menu>
+    </>
   )
 }
 
