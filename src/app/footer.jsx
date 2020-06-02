682ea2cbb016dc5c34c48a25997d5140b01db173
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-06-02 11:02:02 +0200

    improved error handling and display errors to the user

--- src/app/footer.jsx
+++ src/app/footer.jsx
@@ -10,6 +10,20 @@ import DialogActions from "@material-ui/core/DialogActions"
 import DialogContent from "@material-ui/core/DialogContent"
 import DialogContentText from "@material-ui/core/DialogContentText"
 
+const DisplayErrors = ({ errors }) => {
+  const style = { color: "red" }
+  if (errors.ft4.length === 0 && errors.tsh.length === 0) {
+    return null
+  } else {
+    return (
+      <>
+        {errors.ft4.length > 0 && errors.ft4.map((err, i) => <DialogContentText key={i} style={style}>{`ft4: ${err}`}</DialogContentText>)}
+        {errors.tsh.length > 0 && errors.tsh.map((err, i) => <DialogContentText key={i} style={style}>{`tsh: ${err}`}</DialogContentText>)}
+      </>
+    )
+  }
+}
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
@@ -28,16 +42,19 @@ const Footer = ({ dispatch }) => {
   const [open, setOpen] = useState(false)
   const [ft4, setFt4] = useState("")
   const [tsh, setTsh] = useState("")
+  const [errors, setErrors] = useState({ ft4: [], tsh: [] })
 
   const handleClickOpen = () => {
     setOpen(true)
   }
 
   const handleClose = () => {
+    cleanState()
     setOpen(false)
   }
 
   const cleanState = () => {
+    setErrors({ ft4: [], tsh: [] })
     setFt4("")
     setTsh("")
   }
@@ -45,13 +62,23 @@ const Footer = ({ dispatch }) => {
   const handleSubmit = e => {
     e.preventDefault()
 
-    if (!isNaN(ft4) || !isNaN(tsh)) {
+    const ft4Errors = validate(ft4)
+    const tshErrors = validate(tsh)
+
+    if (ft4Errors.length === 0 && tshErrors.length === 0) {
       dispatch({ type: "ADD", payload: { ft4, tsh } })
-      cleanState()
       handleClose()
     } else {
-      console.log(`ft4: ${ft4}, tsh: ${tsh}`)
+      setErrors({ ft4: [...ft4Errors], tsh: [...tshErrors] })
     }
+  }
+
+  const validate = input => {
+    const errors = []
+    if (isNaN(input)) errors.push("invoer is geen nummer")
+    if (input <= 0) errors.push("invoer moet hoger dan 0 zijn")
+
+    return errors
   }
 
   return (
@@ -71,6 +98,7 @@ const Footer = ({ dispatch }) => {
         <form onSubmit={handleSubmit}>
           <DialogContent>
             <DialogContentText>Voeg uw FT4 en TSH labwaarde toe.</DialogContentText>
+            <DisplayErrors errors={errors} />
             <TextField autoFocus margin="dense" id="ft4" label="FT4 labwaarde" type="text" fullWidth variant="outlined" value={ft4} onChange={e => setFt4(e.target.value)} />
             <TextField margin="dense" id="tsh" label="TSH labwaarde" type="text" fullWidth variant="outlined" value={tsh} onChange={e => setTsh(e.target.value)} />
           </DialogContent>
