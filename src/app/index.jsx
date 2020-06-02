da8c22f464cda2bd6183c80c6fcc83f0dbe039ca
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-06-02 12:14:15 +0200

    calculate setpoint added to reducer

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -22,6 +22,45 @@ const reducer = (state, action) => {
 
       return { ...state, values: [...newValues] }
 
+    case "CALC_SETPOINT":
+      // if there is not enough data return without doing anything
+      if (state.values < 2) {
+        console.log("There is not enough data to calculate the setpoint.")
+        return { ...state }
+      }
+
+      // format the data from state for ease of use
+      const data = state.values
+        .reduce((acc, item) => {
+          acc.push([Number(item.ft4), Number(item.tsh)])
+          return acc
+        }, [])
+        .sort(sort2DArray)
+
+      const regression = new ExponentialRegression(
+        data.map(p => p[0]),
+        data.map(p => p[1])
+      )
+
+      const { r2 } = regression.score(
+        data.map(p => p[0]),
+        data.map(p => p[1])
+      )
+
+      const setpointTSH = calcTshSetpoint(regression.A)
+      const setpointFT4 = calcFt4Setpoint(regression.A, regression.B)
+
+      return {
+        ...state,
+        setpoint: {
+          slope: regression.A,
+          multiplier: regression.B,
+          ft4: setpointFT4,
+          tsh: setpointTSH,
+          probability: r2,
+        },
+      }
+
     default:
       throw new Error("Please provide and action")
   }
