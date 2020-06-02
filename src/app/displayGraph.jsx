e8fdede9d9eaab4ae9496adb83a7b32fc1ec5ec7
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-06-02 18:45:19 +0200

    moved state to global reducer

--- src/app/displayGraph.jsx
+++ src/app/displayGraph.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react"
+import React, { useRef, useEffect } from "react"
 import Chart from "chart.js"
 import { decreasingNumbersArray } from "../math/array"
 import { ft4ToTSH } from "../math/thyroid"
@@ -13,83 +13,75 @@ const createSmoothLine = (slope, multiplier) => {
 
 const DisplayGraph = ({ state }) => {
   const canvasRef = useRef()
-  const [minFT4, setMinFT4] = useState(Math.min(...state.values.map(o => o.ft4), 1000) * 0.9)
-  const [maxFT4, setMaxFT4] = useState(Math.max(...state.values.map(o => o.ft4), 0) * 1.05)
-  const [minTSH, setMinTSH] = useState(0)
-  const [maxTSH, setMaxTSH] = useState(Math.max(...state.values.map(o => o.tsh), 0) * 1.1)
 
   useEffect(() => {
-    const chart = new Chart(
-      canvasRef.current.getContext("2d"),
-      {
-        type: "scatter",
-        options: {
-          scales: {
-            xAxes: [
-              {
-                ticks: {
-                  min: minFT4,
-                  max: maxFT4,
-                },
-                scaleLabel: {
-                  display: true,
-                  labelString: "FT4",
-                },
+    const chart = new Chart(canvasRef.current.getContext("2d"), {
+      type: "scatter",
+      options: {
+        scales: {
+          xAxes: [
+            {
+              ticks: {
+                min: state.graph.minFT4,
+                max: state.graph.maxFT4,
               },
-            ],
-            yAxes: [
-              {
-                ticks: {
-                  min: minTSH,
-                  max: maxTSH,
-                },
-                scaleLabel: {
-                  display: true,
-                  labelString: "TSH",
-                },
+              scaleLabel: {
+                display: true,
+                labelString: "FT4",
               },
-            ],
-          },
-        },
-        data: {
-          datasets: [
-            {
-              label: "Setpoint",
-              data: [{ x: state.setpoint.ft4, y: state.setpoint.tsh }],
-              pointBackgroundColor: "green",
-              pointBorderColor: "green",
-              borderColor: "green",
-              fill: false,
-              pointRadius: 10,
-              borderWidth: 5,
-              pointStyle: "crossRot",
             },
+          ],
+          yAxes: [
             {
-              label: "Invoer",
-              data: state.values.map(set => ({ x: set.ft4, y: set.tsh })),
-              pointBackgroundColor: "rgba(1,1,1,1)",
-              borderColor: "rgba(1,1,1,1)",
-              fill: false,
-            },
-            {
-              label: "Berekening",
-              data: createSmoothLine(state.setpoint.slope, state.setpoint.multiplier),
-              pointBackgroundColor: "rgba(1,1,250,1)",
-              borderColor: "rgba(0,0,250, 0.6)",
-              showLine: true,
-              fill: false,
-              lineTension: 0.5,
-              pointRadius: 0,
+              ticks: {
+                min: state.graph.minTSH,
+                max: state.graph.maxTSH,
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "TSH",
+              },
             },
           ],
         },
       },
-      [state]
-    )
+      data: {
+        datasets: [
+          {
+            label: "Setpoint",
+            data: [{ x: state.setpoint.ft4, y: state.setpoint.tsh }],
+            pointBackgroundColor: "green",
+            pointBorderColor: "green",
+            borderColor: "green",
+            fill: false,
+            pointRadius: 10,
+            borderWidth: 5,
+            pointStyle: "crossRot",
+          },
+          {
+            label: "Invoer",
+            data: state.values.map(set => ({ x: set.ft4, y: set.tsh })),
+            pointBackgroundColor: "rgba(1,1,1,1)",
+            borderColor: "rgba(1,1,1,1)",
+            fill: false,
+          },
+          {
+            label: "Berekening",
+            data: createSmoothLine(state.setpoint.slope, state.setpoint.multiplier),
+            pointBackgroundColor: "rgba(1,1,250,1)",
+            borderColor: "rgba(0,0,250, 0.6)",
+            showLine: true,
+            fill: false,
+            lineTension: 0.5,
+            pointRadius: 0,
+          },
+        ],
+      },
+    })
 
     // clean-up before next render
     return () => chart.destroy()
-  }, [state, minFT4, maxFT4, maxTSH])
+  }, [state])
 
   return <canvas ref={canvasRef} />
 }
