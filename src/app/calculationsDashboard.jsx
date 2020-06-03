011a8822e4558438db5122ba03c3f37851c166b3
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-06-03 11:20:23 +0200

    renamed file

--- src/app/calculationsDashboard.jsx
+++ src/app/calculationsDashboard.jsx
@@ -2,14 +2,10 @@ import React, { useEffect } from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import Typography from "@material-ui/core/Typography"
 import Grid from "@material-ui/core/Grid"
-import Table from "@material-ui/core/Table"
-import TableContainer from "@material-ui/core/TableContainer"
-import TableBody from "@material-ui/core/TableBody"
-import TableHead from "@material-ui/core/TableHead"
-import TableCell from "@material-ui/core/TableCell"
-import TableRow from "@material-ui/core/TableRow"
 import Paper from "@material-ui/core/Paper"
 import DisplayGraph from "./displayGraph"
+import GraphController from "./graphController"
+import SetpointTable from "./setpointTable"
 
 const errorInCalculations = setpoint => {
   // if it passed al validations this function returns false
@@ -33,10 +29,6 @@ const useStyles = makeStyles(theme => ({
   noValues: {
     padding: theme.spacing(2),
   },
-  table: {
-    margin: `${theme.spacing(2)}px auto`,
-    maxWidth: 235,
-  },
   graph: {
     margin: `${theme.spacing(0.5)}px ${theme.spacing(2)}px ${theme.spacing(2)}px`,
     paddingTop: theme.spacing(3),
@@ -45,22 +37,17 @@ const useStyles = makeStyles(theme => ({
     paddingLeft: theme.spacing(2),
     maxWidth: 1050,
   },
-  header: {
-    padding: theme.spacing(1),
-    textAlign: "center",
-  },
 }))
 
-const DisplaySetpoint = ({ state, dispatch }) => {
+const CalculationsDashboard = ({ state, dispatch }) => {
   const classes = useStyles()
-  const { setpoint } = state
 
   useEffect(() => {
     if (state.values.length === 0) dispatch({ type: "SWITCH_SCREEN", payload: "user-input" })
     if (state.values.length > 1) dispatch({ type: "CALC_SETPOINT" })
   }, [state.values])
 
-  if (errorInCalculations(setpoint)) {
+  if (errorInCalculations(state.setpoint)) {
     return (
       <Typography align="center" component="h2" variant="h6" className={classes.noValues}>
         Helaas, op basis van deze gegevens is uw setpoint niet te berekenen.
@@ -71,25 +58,10 @@ const DisplaySetpoint = ({ state, dispatch }) => {
   return (
     <Grid container justify="center">
       <Grid item xs={12}>
-        <TableContainer component={Paper} className={classes.table}>
-          <Typography className={classes.header}>Berekend setpoint</Typography>
-          <Table size="small">
-            <TableBody>
-              <TableRow>
-                <TableCell>FT4:</TableCell>
-                <TableCell align="right">{Number(setpoint.ft4).toFixed(2)}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>TSH:</TableCell>
-                <TableCell align="right">{Number(setpoint.tsh).toFixed(2)}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Passingsgraad:</TableCell>
-                <TableCell align="right">{`${Number(setpoint.r2 * 100).toFixed(2)}%`}</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <SetpointTable setpoint={state.setpoint} />
+      </Grid>
+      <Grid item xs={12}>
+        <GraphController dispatch={dispatch} graphSettings={state.graph} />
       </Grid>
       <Grid item xs={12} component={Paper} className={classes.graph}>
         <DisplayGraph state={state} />
@@ -98,4 +70,4 @@ const DisplaySetpoint = ({ state, dispatch }) => {
   )
 }
 
-export default DisplaySetpoint
+export default CalculationsDashboard
