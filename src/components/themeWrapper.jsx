8d1a61561779ecb328046f18e668264dd850fa44
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-06-08 13:36:38 +0200

    changed app styling

--- src/components/themeWrapper.jsx
+++ src/components/themeWrapper.jsx
@@ -4,31 +4,43 @@ import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles"
 import CssBaseline from "@material-ui/core/CssBaseline"
 
 const toolbarHeight = 50
-const primaryColor = "#69469B"
-const secondaryColor = "#cf8fd2"
+const primaryColor = "#292A2C"
+const secondaryColor = "#2DA3D3"
 
 const ThemeWrapper = ({ children }) => {
   const defaultTheme = createMuiTheme()
   const theme = createMuiTheme({
     palette: {
-      // primary: {
-      //   main: primaryColor,
-      // },
-      // secondary: {
-      //   main: secondaryColor,
-      // },
-      // info: {
-      //   main: secondaryColor,
-      // },
+      primary: {
+        main: primaryColor,
+      },
+      secondary: {
+        main: secondaryColor,
+      },
     },
     mixins: {
       toolbar: {
         minHeight: toolbarHeight,
+        [[defaultTheme.breakpoints.down("sm")]]: {
+          minHeight: toolbarHeight + 20,
+        },
         [`${defaultTheme.breakpoints.up("xs")} and (orientation: landscape)`]: {
           minHeight: toolbarHeight - 8,
         },
         [[defaultTheme.breakpoints.up("sm")]]: {
           minHeight: toolbarHeight + 8,
+        },
+      },
+    },
+    overrides: {
+      MuiCssBaseline: {
+        "@global": {
+          a: {
+            color: secondaryColor,
+            "&:hover, :focus, :visited": {
+              color: primaryColor,
+            },
+          },
         },
       },
     },
