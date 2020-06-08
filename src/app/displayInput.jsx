3a7acd2616fd5ed84a74c7ddf8f9306c59036ef8
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-06-08 14:32:27 +0200

    added context menu to input card

--- src/app/displayInput.jsx
+++ src/app/displayInput.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import Card from "@material-ui/core/Card"
 import CardActions from "@material-ui/core/CardActions"
@@ -11,6 +11,13 @@ import TableBody from "@material-ui/core/TableBody"
 import TableCell from "@material-ui/core/TableCell"
 import TableRow from "@material-ui/core/TableRow"
 import Paper from "@material-ui/core/Paper"
+import Menu from "@material-ui/core/Menu"
+import MenuItem from "@material-ui/core/MenuItem"
+
+const initialState = {
+  mouseX: null,
+  mouseY: null,
+}
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -28,31 +35,58 @@ const useStyles = makeStyles(theme => ({
 
 const DisplayInput = ({ ft4, tsh, onClick }) => {
   const classes = useStyles()
+  const [contextMenu, setContextMenu] = useState(initialState)
+
+  const openContextMenu = event => {
+    event.preventDefault()
+    setContextMenu({
+      mouseX: event.clientX - 2,
+      mouseY: event.clientY - 4,
+    })
+  }
+
+  const closeContextMenu = () => {
+    setContextMenu(initialState)
+  }
 
   return (
-    <Card className={classes.root}>
-      <CardContent className={classes.content}>
-        <TableContainer component={Paper}>
-          <Table size="small">
-            <TableBody>
-              <TableRow>
-                <TableCell>FT4:</TableCell>
-                <TableCell align="right">{Number(ft4).toFixed(2)}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>TSH:</TableCell>
-                <TableCell align="right">{Number(tsh).toFixed(2)}</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </CardContent>
-      <CardActions>
-        <IconButton onClick={onClick} className={classes.button}>
-          <DeleteIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
+    <>
+      <Card className={classes.root}>
+        <CardContent className={classes.content} onContextMenu={openContextMenu} style={{ cursor: "context-menu" }}>
+          <TableContainer component={Paper}>
+            <Table size="small">
+              <TableBody>
+                <TableRow>
+                  <TableCell>FT4:</TableCell>
+                  <TableCell align="right">{Number(ft4).toFixed(2)}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>TSH:</TableCell>
+                  <TableCell align="right">{Number(tsh).toFixed(2)}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </CardContent>
+        <CardActions>
+          <IconButton onClick={onClick} className={classes.button}>
+            <DeleteIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+
+      <Menu
+        keepMounted
+        open={contextMenu.mouseY !== null}
+        onClose={closeContextMenu}
+        anchorReference="anchorPosition"
+        anchorPosition={
+          contextMenu.mouseY !== null && contextMenu.mouseX !== null ? { top: contextMenu.mouseY, left: contextMenu.mouseX } : undefined
+        }
+      >
+        <MenuItem onClick={onClick}>Verwijder</MenuItem>
+      </Menu>
+    </>
   )
 }
 
