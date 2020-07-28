4d85ee6f7f4096d21300378b93f05d995358961f
Marco Daniël Leguijt <m.leguijt@outlook.com>
2020-07-28 19:48:20 +0200

    cleaned up config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,18 +11,9 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-material-ui`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `thyroid-setpoint-calculator`,
         short_name: `Setpoint Calculator`,
         start_url: `/`,
         background_color: `#663399`,
@@ -37,8 +28,5 @@ module.exports = {
         trackingId: process.env.GA_TRACKING_ID,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
